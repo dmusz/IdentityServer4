e80b918697b83f0bd2bd840c751b4c2bd16e3b63
Dawid <5026306+dmusz@users.noreply.github.com>
2024-02-07 09:47:37 +0100

    Entity Framework query optimization

--- source/IdentityServer4.EntityFramework.Storage/Stores/ClientStore.cs
+++ source/IdentityServer4.EntityFramework.Storage/Stores/ClientStore.cs
@@ -12,67 +12,65 @@ using IdentityServer4.Stores;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServer4.EntityFramework.Stores
+namespace IdentityServer4.EntityFramework.Stores;
+
+/// <summary>
+/// Implementation of IClientStore thats uses EF.
+/// </summary>
+/// <seealso cref="IdentityServer4.Stores.IClientStore" />
+public class ClientStore : IClientStore
 {
     /// <summary>
-    /// Implementation of IClientStore thats uses EF.
+    /// The DbContext.
     /// </summary>
-    /// <seealso cref="IdentityServer4.Stores.IClientStore" />
-    public class ClientStore : IClientStore
+    protected readonly IConfigurationDbContext Context;
+
+    /// <summary>
+    /// The logger.
+    /// </summary>
+    protected readonly ILogger<ClientStore> Logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClientStore"/> class.
+    /// </summary>
+    /// <param name="context">The context.</param>
+    /// <param name="logger">The logger.</param>
+    /// <exception cref="ArgumentNullException">context</exception>
+    public ClientStore(IConfigurationDbContext context, ILogger<ClientStore> logger)
     {
-        /// <summary>
-        /// The DbContext.
-        /// </summary>
-        protected readonly IConfigurationDbContext Context;
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+        Logger = logger;
+    }
 
-        /// <summary>
-        /// The logger.
-        /// </summary>
-        protected readonly ILogger<ClientStore> Logger;
+    /// <summary>
+    /// Finds a client by id
+    /// </summary>
+    /// <param name="clientId">The client id</param>
+    /// <returns>
+    /// The client
+    /// </returns>
+    public virtual async Task<Client> FindClientByIdAsync(string clientId)
+    {
+        Entities.Client client = await Context.Clients
+            .Include(x => x.AllowedCorsOrigins)
+            .Include(x => x.AllowedGrantTypes)
+            .Include(x => x.AllowedScopes)
+            .Include(x => x.Claims)
+            .Include(x => x.ClientSecrets)
+            .Include(x => x.IdentityProviderRestrictions)
+            .Include(x => x.PostLogoutRedirectUris)
+            .Include(x => x.Properties)
+            .Include(x => x.RedirectUris)
+            .Where(x => x.ClientId == clientId)
+            .SingleOrDefaultAsync();
+           
+        if (client == null)
+            return null;
+        var model = client.ToModel();
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ClientStore"/> class.
-        /// </summary>
-        /// <param name="context">The context.</param>
-        /// <param name="logger">The logger.</param>
-        /// <exception cref="ArgumentNullException">context</exception>
-        public ClientStore(IConfigurationDbContext context, ILogger<ClientStore> logger)
-        {
-            Context = context ?? throw new ArgumentNullException(nameof(context));
-            Logger = logger;
-        }
+        Logger.LogDebug("{clientId} found in database: {clientIdFound}", clientId, model != null);
 
-        /// <summary>
-        /// Finds a client by id
-        /// </summary>
-        /// <param name="clientId">The client id</param>
-        /// <returns>
-        /// The client
-        /// </returns>
-        public virtual async Task<Client> FindClientByIdAsync(string clientId)
-        {
-            IQueryable<Entities.Client> baseQuery = Context.Clients
-                .Where(x => x.ClientId == clientId);
-
-            var client = (await baseQuery.ToArrayAsync())
-                .SingleOrDefault(x => x.ClientId == clientId);
-            if (client == null) return null;
-
-            await baseQuery.Include(x => x.AllowedCorsOrigins).SelectMany(c => c.AllowedCorsOrigins).LoadAsync();
-            await baseQuery.Include(x => x.AllowedGrantTypes).SelectMany(c => c.AllowedGrantTypes).LoadAsync();
-            await baseQuery.Include(x => x.AllowedScopes).SelectMany(c => c.AllowedScopes).LoadAsync();
-            await baseQuery.Include(x => x.Claims).SelectMany(c => c.Claims).LoadAsync();
-            await baseQuery.Include(x => x.ClientSecrets).SelectMany(c => c.ClientSecrets).LoadAsync();
-            await baseQuery.Include(x => x.IdentityProviderRestrictions).SelectMany(c => c.IdentityProviderRestrictions).LoadAsync();
-            await baseQuery.Include(x => x.PostLogoutRedirectUris).SelectMany(c => c.PostLogoutRedirectUris).LoadAsync();
-            await baseQuery.Include(x => x.Properties).SelectMany(c => c.Properties).LoadAsync();
-            await baseQuery.Include(x => x.RedirectUris).SelectMany(c => c.RedirectUris).LoadAsync();
-
-            var model = client.ToModel();
-
-            Logger.LogDebug("{clientId} found in database: {clientIdFound}", clientId, model != null);
-
-            return model;
-        }
+        return model;
+            
     }
 }
